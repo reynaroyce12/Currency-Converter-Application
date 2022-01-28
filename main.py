dd5a41fc452c1c1f56a26e0dc07406c8ef927521
Reyna Royce <reynaroyce.12@gmail.com>
2022-01-29 00:00:38 +0530

    initial

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+parameters = {
+    'access_key': os.getenv('API_KEY')
+}
+
+response = requests.get(url='http://api.exchangeratesapi.io/v1/latest', params=parameters)
+data = response.json()
+currency_rates = data['rates']
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/currency', methods=['POST'])
+def get_details():
+    try:
+        fr_country = request.form['fr_country'].upper()
+        to_country = request.form['to_country'].upper()
+        user_amount = request.form['amount']
+        from_amount = currency_rates[to_country] / currency_rates[fr_country]
+        exchanged_rate = float(user_amount) * from_amount
+        return render_template('currency.html', fr_country=fr_country, to_country=to_country, amount=user_amount,
+                               converted=round(exchanged_rate, 2))
+    except:
+        return render_template('404.html')
